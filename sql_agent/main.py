4b04f02665112912f107f177adf952bec801bc64
dony <xuexi_iq0@163.com>
2026-04-13 21:47:18 +0800

    sql

--- sql_agent/main.py
+++ sql_agent/main.py
@@ -2,10 +2,10 @@ from agent import Agent
 from llm import LLM
 
 model_name = "model"
-base_url = "http://1484537100187225.cn-hangzhou.pai-eas.aliyuncs.com/api/predict/xp_list_select_test/v1"
+base_url = "***"
 api_key = "api"
 default_headers = {"Content-Type": "application/json",
-                   "Authorization": "Njk1ZWU5NTA2Yzk1NjJhMTRmOTgxNmE5OWNiOWJiY2JhNDQ3MjJlMQ=="}
+                   "Authorization": "****"}
 
 gen_args = {
     "max_tokens": 512,
